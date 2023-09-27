597dbb91ac760d0a07d489c77cf717649311d422
phucnv2 <nguyenvanphuc20031996@gmail.com>
2023-09-27 22:29:42 +0700

    update

--- src/test/java/com/anhtester/baitap/commom/BaseTestBT.java
+++ src/test/java/com/anhtester/baitap/commom/BaseTestBT.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -85,6 +87,7 @@ public class BaseTestBT {
         }
     }
 
+
     public void captureScreenImage(String imageName) {
         Robot robot = null;
         try {
@@ -159,5 +162,37 @@ public class BaseTestBT {
 //        WebElement element = driver.findElement(By.xpath(locator));
 //        js.executeScript("arguments[0].scrollIntoView(true);", element);
         js.executeScript("window.scrollTo(0,500)");
+    }
+
+    /**
+     * Wait for Page loaded
+     * Chờ đợi trang tải xong (Javascript tải xong)
+     */
+    public static void waitForPageLoaded(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30), Duration.ofMillis(500));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        //Wait for Javascript to load
+        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                return js.executeScript("return document.readyState").toString().equals("complete");
+            }
+        };
+
+        //Check JS is Ready
+        boolean jsReady = js.executeScript("return document.readyState").toString().equals("complete");
+
+        //Wait Javascript until it is Ready!
+        if (!jsReady) {
+            System.out.println("Javascript is NOT Ready.");
+            //Wait for Javascript to load
+            try {
+                wait.until(jsLoad);
+            } catch (Throwable error) {
+                error.printStackTrace();
+                Assert.fail("FAILED. Timeout waiting for page load.");
+            }
+        }
     }
 }
